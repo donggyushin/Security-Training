0827608cd70311cd343625de5f0d3cbe021f9081
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-07-05 22:00:25 +0900

    gist

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -11,12 +11,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception
 	{
-		//web.ignoring().antMatchers("/**");
+		//이런식으로 인증할 것들을 풀어주는 겁니다. 주로 리소스  
+		web.ignoring().antMatchers("/css/**", "/script/**", "/");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.authorizeRequests()		//여기에선 리소르외에 페이지의 인증/비인증/인증권한등을 성정하는게 좋은 것 같습니다. 
 			.antMatchers("/mypage").authenticated()
 			.antMatchers("/**").permitAll();			//순서를 바꿨음. 
 	}
