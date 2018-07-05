43b2b45a01690fa781dd44a87071c6d0854f9e5a
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-07-05 21:41:02 +0900

    partial authenticate

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,5 +12,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception
 	{
 		web.ignoring().antMatchers("/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/mypage").authenticated();
+		// /mypage url에 부분 인증페이지를 줘보자. 
+		//
 	}
 }
