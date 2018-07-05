af75794f2a76a6e5b136aba0a477aaeb831680ee
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-07-05 21:51:08 +0900

    partial authenticate third

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/**").permitAll()				//모든 페이지에 접속 권한을 부여 한 
-			.antMatchers("/mypage").authenticated();	//mypage에다가 부분별로 인증요구 
+			.antMatchers("/mypage").authenticated()
+			.antMatchers("/**").permitAll();			//순서를 바꿨음. 
 	}
 }
